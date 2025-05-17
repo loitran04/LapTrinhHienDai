c9c2d30afb182a22c43b5e027d3fd66fd6adf868
loi.tv04 <tvloi250204@gmail.com>
2025-05-17 19:01:37 +0700

    4

--- findJobApp/apiJob/settings.py
+++ findJobApp/apiJob/settings.py
@@ -79,8 +79,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_SMTP = 'shopnro247combot@gmail.com'
-EMAIL_SMTP_PASSWORD = 'ilku tthj tfat icjg'
+EMAIL_HOST_USER = 'shopnro247combot@gmail.com'
+EMAIL_HOST_PASSWORD = 'ilku tthj tfat icjg'
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 
 # Google Maps API Key (for tracking work locations)
